b861ede467d855dd066170265e9d27355816036c
Marvin Reimer <therealmarv@gmail.com>
2013-08-21 10:03:31 +0200

    get all opf referenced files shellscript

---
+++ get-all-opf-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xsltproc extract-href-debug.xsl EPUB/package-Scratch.opf
+xsltproc extract-href-debug.xsl EPUB/package-ieb-skeleton.opf
+xsltproc extract-href-debug.xsl EPUB/package-kathi-scratch.opf
+xsltproc extract-href-debug.xsl EPUB/package-physics-11.opf
+xsltproc extract-href-debug.xsl EPUB/package-physics-12.opf
+xsltproc extract-href-debug.xsl EPUB/package-physics-cnx.opf
